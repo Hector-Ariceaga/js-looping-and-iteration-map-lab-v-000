f5a0ef66f3de931a7e1f6f5886bb846181e1c1aa
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-09 17:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   const names = drivers.map(function(driver) {
-    debugger
+    debugger;
     return Object.assign({}, driver, {firstName: driver, lastName: driver})})
   return names
 }
